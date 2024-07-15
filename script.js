5d6f2326dd7b364d3359d94e9f6bfd37f6821ea6
Sherif Fouad <130251496+Sheriff30@users.noreply.github.com>
2024-07-15 05:31:42 +0300

    adding sticky header

--- script.js
+++ script.js
@@ -39,3 +39,17 @@ toggleBtn.addEventListener("click", function (e) {
     e.target.name = "menu-outline";
   }
 });
+
+// Sticky header
+
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("header");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
+});
